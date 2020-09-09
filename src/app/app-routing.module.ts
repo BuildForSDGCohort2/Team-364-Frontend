650f5e4147ee920eb2faf5fc7133edfd9c74c306
Rudy-Menekam <menekamrudy@gmail.com>
2020-09-09 18:45:25 +0100

    new changes (#30)
    Co-authored-by: Rudy-Menekam <[menekamrudy@gmail.com]>
    Co-authored-by: NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ChannelsComponent } from './pages/channels/channels.component';
+import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SinglePosdcastComponent } from './pages/single-posdcast/single-posdcast.component';
 import { PosdcastsCategoriesComponent } from './pages/posdcasts-categories/posdcasts-categories.component';
@@ -7,6 +9,7 @@ import { FeaturesComponent } from './pages/features/features.component';
 
 
 const routes: Routes = [
+  { path: "", component: ChannelsComponent},
   { path: "", component: FeaturesComponent},
   { path: "register", component: RegisterComponent},
   { path: "single-podcast", component: SinglePosdcastComponent},
